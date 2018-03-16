ef018b604a97fa775656fdefe70f33d8cb539994
Mahmoud Abdelkader <mabdelkader@gmail.com>
2018-03-15 22:47:10 -0700

    modularization of the app plus now whitelabeled!

--- demo/app.py
+++ demo/app.py
@@ -1,129 +1,26 @@
 import argparse
-import json
-import sys
-import traceback
+import os
 from urllib.parse import urlunsplit, urlparse
 
-import os
-import requests
-from flask import flash, Flask, render_template, request
-from flask_admin import Admin
-from flask_admin.actions import action
-from flask_admin.contrib.sqla import ModelView
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
 
+import persistence
 import processor
+import payment
 from flask_admin_material import setup_templates as setup_admin_material_theme
 
 
 app = Flask(__name__)
-app.register_blueprint(processor.processor)
-app = setup_admin_material_theme(app)
-app = processor.on_register(app)
-
+# app = setup_admin_material_theme(app)
 
 app.config['SECRET_KEY'] = '123456790'
 app.config['DATABASE_FILE'] = 'pydemo.sqlite'
 app.config['SQLALCHEMY_DATABASE_URI'] =  'sqlite:///' + app.config['DATABASE_FILE']
 app.config['SQLALCHEMY_ECHO'] = True
 
-db = SQLAlchemy(app)
-
-
-class Payment(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    billing_address = db.Column(db.String(100))
-    card_number = db.Column(db.String(100))
-    card_expiration = db.Column(db.String(100))
-    card_security_code = db.Column(db.String(100))
-
-    @classmethod
-    def from_dict(cls, kwargs):
-        payment_obj = cls()
-        payment_obj.name = kwargs['name']
-        payment_obj.billing_address = kwargs['billing_address']
-        payment_obj.card_number = kwargs['card-number']
-        payment_obj.card_expiration = kwargs['card-expiration-date']
-        payment_obj.card_security_code = kwargs['card-security-code']
-        return payment_obj
-
-    def charge(self):
-        response = processor.client.charge({'card': self.card_number})
-        response.raise_for_status()
-        print(response.json())
-        return True
-
-
-class PaymentAdmin(ModelView):
-
-    @action('charge', 'Charge', 'Are you sure you want to charge this card?')
-    def action_charge(self, ids):
-        try:
-            query = Payment.query.filter(Payment.id.in_(ids))
-            count = 0
-            for payment_entry in query.all():
-                payment_entry.charge()
-                count += 1
-            flash('{count} cards were charged successfully.'.format(count=count))
-        except Exception as ex:
-            print(''.join(traceback.format_exception(None,ex, ex.__traceback__)),
-                  file=sys.stderr, flush=True)
-            flash('Failed to approve users. {error}'.format(error=ex), category='error')
-
-
-admin = Admin(app, name='pydemo', template_mode='bootstrap3')
-admin.add_view(PaymentAdmin(Payment, db.session))
-
-def redaction(dic):
-    return render_template('show_redacted.html', data = dic)
-
-@app.route('/', methods=["GET", "POST"])
-def index():
-    if request.method == 'GET':
-        return render_template('index.html')
-    elif request.method =='POST':
-        imm = request.values
-        dic = imm.to_dict(flat=True)
-        json_data = json.dumps(dic)
-        print(json_data)
-        return redaction(dic)
-
-@app.route('/show_redacted', methods=['GET'])
-def show_redacted():
-    render_template('show_redacted.html')
-
-@app.route('/revealed_data', methods=['POST'])
-def revealed_data():
-    imm = request.values['data']
-    data = imm.replace("'", '"')
-    json_data = data
-    print(json_data)
-    requests.post(
-    'https://third-party.herokuapp.com/',
-    data=json_data,
-    headers={"Content-type": "application/json"},
-    proxies={"https": "https://USuENQQdfR8yhgjdz2x11ydY:9be4303d-f84d-4943-a9c1-164e53c6fbac@tntrsf2iagd.SANDBOX.verygoodproxy.com:8080"},
-    verify='demo/static/cert.pem'
-    )
-    # values = r.json()['data']
-    # json_acceptable_string = values.replace("'", "\"")
-    # vals = json.loads(json_acceptable_string)
-    return render_template('index.html')
-
-@app.route('/payment', methods=["GET", "POST"])
-def payment():
-    if request.method == 'GET':
-        return render_template('payment.html')
-    elif request.method =='POST':
-        imm = request.values
-        dic = imm.to_dict(flat=True)
-        payment_entry = Payment.from_dict(dic)
-        db.session.add(payment_entry)
-        db.session.commit()
-        json_data = json.dumps(dic)
-        print(json_data)
-        return redaction(dic)
+persistence.init_app(app)
+processor.init_app(app)
+payment.init_app(app)
 
 
 def create_parser():
@@ -157,15 +54,9 @@ def start_server(parsed_args):
             threaded=True)
 
 
-def init_db(drop=False):
-    if drop:
-        db.drop_all()
-    db.create_all()
-
-
 def main(pa):
-    if pa.init_db:
-        init_db(drop=True)
+    with app.app_context():
+        persistence.init_db(drop=pa.init_db)
 
     pr = ('http','{0}:{1}'.format(pa.server_host, pa.server_port), '',
           None, None)
